ae4b451216ac6340af45660ddc1ffecc71c82899
gong4857 <village1031@gmail.com>
2025-04-25 16:02:25 +0900

    백엔드 스웨거 url 수정

--- src/main/java/com/bucams/bucams/config/swagger/SwaggerConfig.java
+++ src/main/java/com/bucams/bucams/config/swagger/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         // Server server = new Server().url("https://bucams-api.com:30804");
         // Server server = new Server().url("http://gyoungtae.iptime.org:31000");
-        Server server = new Server().url("https://gandalp-alb-812230836.ap-northeast-2.elb.amazonaws.com:8080");
+        Server server = new Server().url("https://api.gandalp-service.com:8080");
         return new OpenAPI()
             .addServersItem(server)
             .info(new Info()
